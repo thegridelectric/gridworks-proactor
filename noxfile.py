abb20d607a5a52d6675a26d071ce9b10c5444348
Andrew Schweitzer <schweitz72@gmail.com>
2024-08-30 17:51:46 -0400

    Enable python 3.12

--- noxfile.py
+++ noxfile.py
@@ -23,7 +23,7 @@ except ImportError:
 
 
 package = "gwproactor"
-python_versions = ["3.11"]
+python_versions = ["3.12", "3.11"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
